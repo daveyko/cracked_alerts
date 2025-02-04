9e9fb90c36dc371e29541186739a3662d4595b52
David Ko <davidko@Davids-MacBook-Pro.local>
2025-02-03 23:17:59 -0800

    format

--- services/cronPruneOldTransactions.js
+++ services/cronPruneOldTransactions.js
@@ -3,28 +3,28 @@ const TRANSACTION_EXPIRY = 10 * 60 * 1000; // Transactions older than 10 minutes
 
 function pruneOldTransactions(cache) {
     const now = Date.now();
-    console.log("Pruning old transactions. Current cache size:", cache.size());
+    console.log('Pruning old transactions. Current cache size:', cache.size());
     cache.getAll().forEach((entry, key) => {
         // Filter out old transactions
-        entry.transactions = entry.transactions.filter(tx => { 
-            return now - tx.blockTime < TRANSACTION_EXPIRY
+        entry.transactions = entry.transactions.filter((tx) => {
+            return now - tx.blockTime < TRANSACTION_EXPIRY;
         });
         // Remove wallets that no longer have recent transactions
-        entry.uniqueWallets = new Set(entry.transactions.map(tx => tx.walletName));
+        entry.uniqueWallets = new Set(entry.transactions.map((tx) => tx.walletName));
         // If there are no transactions left, remove the entry
         if (entry.transactions.length === 0) {
             cache.del([key]);
         }
     });
-    console.log("Pruned old transactions. Current cache size:", cache.size());
+    console.log('Pruned old transactions. Current cache size:', cache.size());
 }
 
-function pruneCacheStartCron(cache) { 
-    setInterval(() => { 
-        pruneOldTransactions(cache) 
-    } , PRUNE_INTERVAL);
+function pruneCacheStartCron(cache) {
+    setInterval(() => {
+        pruneOldTransactions(cache);
+    }, PRUNE_INTERVAL);
 }
 
-module.exports = { 
-    pruneCacheStartCron
-}
+module.exports = {
+    pruneCacheStartCron,
+};
